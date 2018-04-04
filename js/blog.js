6ba7c0d064243eef6ee0337b72edd5811ecd2ae4
ankitkamboj0 <ankitkamboj0@gmail.com>
2018-04-04 18:02:46 +0530

    added json data

---
+++ js/blog.js
@@ -0,0 +1,22 @@
+const posts = [{
+    "id": 1,
+    "title": "Post 1",
+    "content":"This is blog content",
+    "imgSrc":"img/img1.jpg",
+    "comments":["comment 1","comment 2"]
+
+},{
+    "id": 2,
+    "title": "Post 2",
+    "content":"This is blog content",
+    "imgSrc":"img/img2.jpg",
+    "comments":["comment 1","comment 2","comment 3","comment 4","comment 5","comment 6"]
+
+},{
+    "id": 3,
+    "title": "Post 3",
+    "content":"This is blog content",
+    "imgSrc":"img/img3.jpg",
+    "comments":["comment 1","comment 2","comment 3","comment 4","comment 5","comment 6"]
+
+}]
